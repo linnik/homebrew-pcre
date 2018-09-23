6696e23794fae868fef868d7bec06a4ab140004a
Vyacheslav Linnik <vyacheslav.linnik@gmail.com>
2018-09-23 20:59:08 +0300

    add formula

---
+++ pcre.rb
@@ -0,0 +1,39 @@
+class Pcre < Formula
+  desc "Perl compatible regular expressions library"
+  homepage "https://www.pcre.org/"
+  url "https://ftp.pcre.org/pub/pcre/pcre-8.35.tar.bz2"
+  mirror "https://www.mirrorservice.org/sites/ftp.exim.org/pub/pcre/pcre-8.35.tar.bz2"
+  sha256 "a961c1c78befef263cc130756eeca7b674b4e73a81533293df44e4265236865b"
+
+
+  head do
+    url "svn://vcs.exim.org/pcre/code/trunk"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  def install
+    system "./autogen.sh" if build.head?
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--enable-utf8",
+                          "--enable-pcre8",
+                          "--enable-pcre16",
+                          "--enable-pcre32",
+                          "--enable-unicode-properties",
+                          "--enable-pcregrep-libz",
+                          "--enable-pcregrep-libbz2",
+                          "--enable-jit"
+    system "make"
+    ENV.deparallelize
+    system "make", "test"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/pcregrep", "regular expression", "#{prefix}/README"
+  end
+end
+
